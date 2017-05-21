c36055c1197e25f5169f44daeb2b72fee1160749
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2017-05-21 01:30:22 -0500

    Update: FooterBlade

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="fl-page-footer-wrap" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+	<div class="fl-page-footer">
+		<div class="fl-page-footer-container container">
+			<div class="fl-page-footer-row row">
+				<div class="col-md-6 col-sm-6 text-left clearfix">
+				<div class="fl-page-footer-text fl-page-footer-text-1">COPYRIGHT Viva La Via 2017 | 110 E. HOUSTON, SAN ANTONIO, TX 78205</div>
+				</div>			
+				<div class="col-md-6 col-sm-6 text-right clearfix">
+				<div class="fl-page-footer-text fl-page-footer-text-2">• <a href="mailto:info@TECHSTIZO.COM">info@VivaLaVia.com</a></div>
+			</div>		
+		</div>
+	</div>
+</div>	
+</footer>
+</div>
