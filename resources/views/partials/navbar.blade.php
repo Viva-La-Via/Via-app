71b051502d3075bdefa6234b974f74ae4ed6c8f8
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2017-05-21 00:57:29 -0500

    Update: Navbar isn't firing.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,20 +1,11 @@
-<div class="fl-page-nav-wrap">
-	<div class="fl-page-nav-container container">
-		<nav class="fl-page-nav navbar navbar-default" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".fl-page-nav-collapse">
-				<span>Menu</span>
-			</button>
+<div class="fl-page-fixed-nav-wrap col-md-9 col-sm-12">
+	<div class="fl-page-nav-wrap">
+		<nav class="fl-page-nav fl-nav navbar navbar-default" role="navigation">
 			<div class="fl-page-nav-collapse collapse navbar-collapse">
 				<ul class="fl-page-header-nav nav navbar-nav menu">
-				<li><a href="http://techstizo.com/">Home</a></li>
-				</ul>
-			<div class="fl-page-nav-search">
-				<a href="javascript:void(0);" class="fa fa-search"></a>
-				<form method="get" role="search" action="http://techstizo.com/" title="Type and press Enter to search.">
-					<input type="text" class="fl-search-input form-control" name="s" placeholder="Search" value="">
-				</form>
-			</div>				
-		</div>
+					<li><a href="http://techstizo.com/">Home</a></li>
+				</ul>							
+			</div>
 		</nav>
 	</div>
 </div>
