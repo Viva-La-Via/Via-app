08449a20e82e3c26ed48818f0434c4670168c452
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2017-05-21 01:24:52 -0500

    Update: Content area.

---
+++ resources/views/partials/content.blade.php
@@ -0,0 +1,413 @@
+<div class="fl-page-content" itemprop="mainContentOfPage">
+	
+		
+<div class="fl-content-full container">
+	<div class="row">
+		<div class="fl-content col-md-12">
+							<article class="fl-post post-173 page type-page status-publish hentry" id="fl-post-173" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">
+
+	
+	<div class="fl-post-content clearfix" itemprop="text">
+		<div class="fl-builder-content fl-builder-content-173 fl-builder-content-primary fl-builder-global-templates-locked" data-post-id="173"><div class="fl-row fl-row-fixed-width fl-row-bg-none fl-node-5920fbe616c86" data-node="5920fbe616c86">
+	<div class="fl-row-content-wrap">
+				<div class="fl-row-content fl-row-fixed-width fl-node-content">
+		
+<div class="fl-col-group fl-node-5920fbe6193cb" data-node="5920fbe6193cb">
+			<div class="fl-col fl-node-5920fbe619671 fl-col-has-cols" data-node="5920fbe619671" style="width: 100%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-video fl-node-5920fbeba7abd .movie" data-node="5920fbeba7abd" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		
+<div class="fl-video fl-wp-video" itemscope itemtype="http://schema.org/VideoObject">
+	<meta itemprop="url" content="http://techstizo.com/wp-content/uploads/2017/05/via.mp4" /><meta itemprop="thumbnail" content="" /><div style="width: 100px;" class="wp-video"><!--[if lt IE 9]><script>document.createElement('video');</script><![endif]-->
+<video class="wp-video-shortcode" id="video-173-1" width="100" height="100" loop="1" autoplay="1" preload="metadata" controls="controls"><source type="video/mp4" src="http://techstizo.com/wp-content/uploads/2017/05/via.mp4?_=1" /><a href="http://techstizo.com/wp-content/uploads/2017/05/via.mp4">http://techstizo.com/wp-content/uploads/2017/05/via.mp4</a></video></div></div>	</div>
+</div>
+<div class="fl-col-group fl-node-5920fe3d70af1 fl-col-group-nested" data-node="5920fe3d70af1">
+			<div class="fl-col fl-node-5920fe3d70edf fl-col-small" data-node="5920fe3d70edf" style="width: 50%;">
+	<div class="fl-col-content fl-node-content">
+		</div>
+</div>			<div class="fl-col fl-node-5920fe3d70f65 fl-col-small hero" data-node="5920fe3d70f65" style="width: 50%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-html fl-node-5920fe45464ac" data-node="5920fe45464ac" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-html">
+	<div class="toppers" style="overflow-y:scroll; height:390px; opacity: .7;">
+    <h2><strong>Directions to San Antonio Clubhouse</strong></h2>
+    
+    <h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+        <h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+    
+		</div>
+	</div>	
+</div>
+</div>	
+</div>
+</div>	
+</div>
+
+<div class="fl-col-group fl-node-5920ff355585a fl-col-group-nested" data-node="5920ff355585a">
+	<div class="fl-col fl-node-5920ff3555c36" data-node="5920ff3555c36" style="width: 100%;">
+		<div class="fl-col-content fl-node-content">
+		<div class="fl-module fl-module-heading fl-node-5920ffa0b9eda" data-node="5920ffa0b9eda" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">TRENDING</span>
+		</h3>	
+	</div>
+</div>	
+</div>
+</div>	
+</div>
+</div>
+</div>	
+</div>
+
+<div class="fl-col-group fl-node-592101e289b86" data-node="592101e289b86">
+			<div class="fl-col fl-node-592101e289f7d fl-col-has-cols" data-node="592101e289f7d" style="width: 100%;">
+	<div class="fl-col-content fl-node-content">
+	
+<div class="fl-col-group fl-node-5921026506cba fl-col-group-nested" data-node="5921026506cba">
+			<div class="fl-col fl-node-59210265070d1 fl-col-small" data-node="59210265070d1" style="width: 33.33%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-heading fl-node-59210f1ce5178" data-node="59210f1ce5178" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">GRAPH</span>
+	</h3>	</div>
+</div><div class="fl-module fl-module-separator fl-node-5921077cf02dd" data-node="5921077cf02dd" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-separator"></div>	</div>
+</div><div class="fl-module fl-module-photo fl-node-59210823b6531" data-node="59210823b6531" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-photo fl-photo-align-center" itemscope itemtype="http://schema.org/ImageObject">
+	<div class="fl-photo-content fl-photo-img-jpg">
+				<img class="fl-photo-img wp-image-78 size-full" src="http://techstizo.com/wp-content/uploads/2017/05/brunch.jpg" alt="brunch" itemprop="image" srcset="http://techstizo.com/wp-content/uploads/2017/05/brunch.jpg 1125w, http://techstizo.com/wp-content/uploads/2017/05/brunch-300x200.jpg 300w, http://techstizo.com/wp-content/uploads/2017/05/brunch-768x512.jpg 768w, http://techstizo.com/wp-content/uploads/2017/05/brunch-1024x683.jpg 1024w" sizes="(max-width: 1125px) 100vw, 1125px" />
+		    
+			</div>
+	</div>	
+	</div>
+</div>	
+</div>
+</div>			
+	<div class="fl-col fl-node-5921026507137 fl-col-small" data-node="5921026507137" style="width: 33.34%;">
+		<div class="fl-col-content fl-node-content">
+		<div class="fl-module fl-module-heading fl-node-5921028908478" data-node="5921028908478" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">LINE GRAPH</span>
+		</h3>	
+		</div>
+	</div>
+<div class="fl-module fl-module-separator fl-node-592107528ed73" data-node="592107528ed73" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-separator"></div>	
+	</div>
+	</div>
+<div class="fl-module fl-module-photo fl-node-592108079ae25" data-node="592108079ae25" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-photo fl-photo-align-center" itemscope itemtype="http://schema.org/ImageObject">
+	<div class="fl-photo-content fl-photo-img-jpg">
+				<img class="fl-photo-img wp-image-78 size-full" src="http://techstizo.com/wp-content/uploads/2017/05/brunch.jpg" alt="brunch" itemprop="image" srcset="http://techstizo.com/wp-content/uploads/2017/05/brunch.jpg 1125w, http://techstizo.com/wp-content/uploads/2017/05/brunch-300x200.jpg 300w, http://techstizo.com/wp-content/uploads/2017/05/brunch-768x512.jpg 768w, http://techstizo.com/wp-content/uploads/2017/05/brunch-1024x683.jpg 1024w" sizes="(max-width: 1125px) 100vw, 1125px" />
+		    
+			</div>
+		</div>	
+	</div>
+</div>	
+</div>
+</div>			
+	<div class="fl-col fl-node-5921026507197 fl-col-small" data-node="5921026507197" style="width: 33.33%;">
+		<div class="fl-col-content fl-node-content">
+		<div class="fl-module fl-module-heading fl-node-592102b318c7d" data-node="592102b318c7d" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">AVERAGE FARES</span>
+		</h3>			
+	</div>
+</div>
+	<div class="fl-module fl-module-separator fl-node-59210778745ba" data-node="59210778745ba" data-animation-delay="0.0">
+		<div class="fl-module-content fl-node-content">
+			<div class="fl-separator"></div>		
+		</div>
+	</div>
+<div class="fl-module fl-module-photo fl-node-5921081e7ceae" data-node="5921081e7ceae" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-photo fl-photo-align-center" itemscope itemtype="http://schema.org/ImageObject">
+	<div class="fl-photo-content fl-photo-img-jpg">
+				<img class="fl-photo-img wp-image-78 size-full" src="http://techstizo.com/wp-content/uploads/2017/05/brunch.jpg" alt="brunch" itemprop="image" srcset="http://techstizo.com/wp-content/uploads/2017/05/brunch.jpg 1125w, http://techstizo.com/wp-content/uploads/2017/05/brunch-300x200.jpg 300w, http://techstizo.com/wp-content/uploads/2017/05/brunch-768x512.jpg 768w, http://techstizo.com/wp-content/uploads/2017/05/brunch-1024x683.jpg 1024w" sizes="(max-width: 1125px) 100vw, 1125px" />
+		    
+			</div>
+		</div>	
+	</div>
+</div>	
+</div>
+</div>	
+</div>
+</div>
+</div>	
+</div>
+
+<div class="fl-col-group fl-node-59210a2c32094" data-node="59210a2c32094">
+			<div class="fl-col fl-node-59210a2c32574" data-node="59210a2c32574" style="width: 100%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-heading fl-node-59210a2071e65" data-node="59210a2071e65" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">ROUTE ADHERANCE</span>
+	</h3>	</div>
+</div>	</div>
+</div>	</div>
+
+<div class="fl-col-group fl-node-59210ace787e2" data-node="59210ace787e2">
+			<div class="fl-col fl-node-59210ace78cbf fl-col-has-cols" data-node="59210ace78cbf" style="width: 100%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-rich-text fl-node-59210b8394e3e adheranceText" data-node="59210b8394e3e" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-rich-text">
+	<p>Look at the sunset, life is amazing, life is beautiful, life is what you make it. Life is what you make it, so let’s make it. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! The key is to enjoy life, because they don’t want you to enjoy life. I promise you, they don’t want you to jetski, they don’t want you to smile.</p>
+</div>	
+</div>
+</div>
+<div class="fl-col-group fl-node-59210bdaa20f9 fl-col-group-nested" data-node="59210bdaa20f9">
+			<div class="fl-col fl-node-59210bdaa25fb fl-col-small" data-node="59210bdaa25fb" style="width: 33.33%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-heading fl-node-5921026c2b220" data-node="5921026c2b220" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">SCHEDULED TIME</span>
+		</h3>	
+	</div>
+</div>
+	<div class="fl-module fl-module-separator fl-node-59210f820ebd3" data-node="59210f820ebd3" data-animation-delay="0.0">
+		<div class="fl-module-content fl-node-content">
+			<div class="fl-separator"></div>	
+		</div>
+</div>
+<div class="fl-module fl-module-html fl-node-59210c8e0203e" data-node="59210c8e0203e" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-html">
+	<div class="adherance" style="overflow-y:scroll; height:390px; opacity: .7;">
+<h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+        <h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+</div>
+</div>	
+</div>
+</div>	
+</div>
+</div>			
+<div class="fl-col fl-node-59210bdaa2663 fl-col-small" data-node="59210bdaa2663" style="width: 33.33%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-heading fl-node-59210f328f0c3" data-node="59210f328f0c3" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">ARRIVAL TIME</span>
+	</h3>	
+	</div>
+</div>
+	<div class="fl-module fl-module-separator fl-node-59210f8f9a8da" data-node="59210f8f9a8da" data-animation-delay="0.0">
+		<div class="fl-module-content fl-node-content">
+			<div class="fl-separator">		
+			</div>	
+		</div>
+	</div>
+<div class="fl-module fl-module-html fl-node-59210c72d616c" data-node="59210c72d616c" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-html">
+			<div class="adherance" style="overflow-y:scroll; height:390px; opacity: .7;">
+			<h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+        <h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+		</div>        
+	</div>	
+</div>
+</div>	
+</div>
+</div>			
+	<div class="fl-col fl-node-59210bdaa26c4 fl-col-small" data-node="59210bdaa26c4" style="width: 33.33%;">
+		<div class="fl-col-content fl-node-content">
+		<div class="fl-module fl-module-heading fl-node-59210f29c4dbc" data-node="59210f29c4dbc" data-animation-delay="0.0">
+		<div class="fl-module-content fl-node-content">
+			<h3 class="fl-heading">
+		<span class="fl-heading-text">DEPARTURE TIME</span>
+			</h3>	
+		</div>
+	</div>
+	<div class="fl-module fl-module-separator fl-node-59210f9389d35" data-node="59210f9389d35" data-animation-delay="0.0">
+		<div class="fl-module-content fl-node-content">
+			<div class="fl-separator"></div>	
+		</div>
+	</div>
+<div class="fl-module fl-module-html fl-node-59210c83ef5f4" data-node="59210c83ef5f4" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-html">
+	<div class="adherance" style="overflow-y:scroll; height:390px; opacity: .7;">
+	<h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+        <h3><em>Downtown, San Antonio, TX</em></h3>
+        <ol>
+            <li>Get on I-10 W from E Martin St</li>
+                <h6>5 min (1.0 mi)</h6>
+     
+            <li>Follow I-10 W to Interstate 10 Frontage Rd. Take exit 563 from I-10 W</li>
+                <h6>7 min (6.8 mi)</h6>
+     
+            <li>Continue on Interstate 10 Frontage Rd. Drive to Citizens Pkwy</li>
+                <h6>4 min (1.7 mi)</h6>
+                
+            <li>Turn right onto Citizens Pkwy</li>
+                <h6>Your Destination is on the left</h6>
+                <h5>San Antonio Clubhouse</h5>
+                <h6>6851 Citizens Pkwy #100 San Antonio, TX 78229</h6>
+        </ol>
+		</div>        
+	</div>	
+	</div>
+</div>	
+</div>
+</div>	
+</div>
+
+<div class="fl-col-group fl-node-592115ea63ced fl-col-group-nested" data-node="592115ea63ced">
+			<div class="fl-col fl-node-592115ea64232" data-node="592115ea64232" style="width: 100%;">
+	<div class="fl-col-content fl-node-content">
+	<div class="fl-module fl-module-heading fl-node-5921165ddfbfb" data-node="5921165ddfbfb" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<h3 class="fl-heading">
+		<span class="fl-heading-text">LINE GRAPH OF ROUTE ADHERANCE</span>
+		</h3>	
+		</div>
+	</div>
+	<div class="fl-module fl-module-photo fl-node-592115f4b91ef" data-node="592115f4b91ef" data-animation-delay="0.0">
+	<div class="fl-module-content fl-node-content">
+		<div class="fl-photo fl-photo-align-center" itemscope itemtype="http://schema.org/ImageObject">
+	<div class="fl-photo-content fl-photo-img-png">
+				<img class="fl-photo-img wp-image-190 size-full" src="http://techstizo.com/wp-content/uploads/2017/05/line_graph.png" alt="line_graph" itemprop="image" srcset="http://techstizo.com/wp-content/uploads/2017/05/line_graph.png 730w, http://techstizo.com/wp-content/uploads/2017/05/line_graph-300x132.png 300w" sizes="(max-width: 730px) 100vw, 730px" />
+		    
+			</div>
+		</div>	
+	</div>
+</div>	
+</div>
+</div>	
+</div>
+</div>
+</div>	
+</div>
+</div>
+</div>
+</div>
+</div>	
+</div>
+<!-- .fl-post-content -->
+
+	
+</article>
+<!-- .fl-post -->					
+	</div>
+	</div>
+</div>
+
+			
+</div>
